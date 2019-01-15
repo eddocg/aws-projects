4f002488b24b827a5e3f18ea40bc3ee9396aebf2
Edmundo <egallegos@express.com>
2019-01-15 10:48:24 -0500

    camel sqs added

---
+++ camel-sqs-consumer/src/main/java/com/eddocg/aws/sqs/App.java
@@ -0,0 +1,13 @@
+package com.eddocg.aws.sqs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
